abd5c6f6a69e92ca47f1318b3da08db7bcc67926
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2025-04-15 15:46:08 +0200

    Add initial project

---
+++ src/Umbraco.Test.Search.Examine.Unit/Tests/IndexTests.cs
@@ -0,0 +1,15 @@
+﻿namespace Umbraco.Test.Search.Examine.Unit.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
