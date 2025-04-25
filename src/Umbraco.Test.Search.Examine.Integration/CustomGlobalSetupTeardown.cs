0e0b8acd93575d8d11895577d6b790a6d8b566e5
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2025-04-25 09:26:47 +0200

    Add integration tests

---
+++ src/Umbraco.Test.Search.Examine.Integration/CustomGlobalSetupTeardown.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace Umbraco.Test.Search.Examine.Integration;
+
+[SetUpFixture]
+public class CustomGlobalSetupTeardown
+{
+    private static GlobalSetupTeardown _setupTearDown;
+
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        _setupTearDown = new GlobalSetupTeardown();
+        _setupTearDown.SetUp();
+    }
+
+    [OneTimeTearDown]
+    public void TearDown()
+    {
+        _setupTearDown.TearDown();
+    }
+}
---
+++ src/Umbraco.Test.Search.Examine.Integration/Tests/IndexServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Umbraco.Cms.Tests.Common.Testing;
+using Umbraco.Cms.Tests.Integration.Testing;
+
+namespace Umbraco.Test.Search.Examine.Integration.Tests;
+
+[TestFixture]
+[UmbracoTest(Database = UmbracoTestOptions.Database.NewSchemaPerFixture)]
+public class IndexServiceTests : UmbracoIntegrationTest
+{
+    [Test]
+    public async Task CanIndexCustomData()
+    {
+        Assert.Pass();
+    }
+}
