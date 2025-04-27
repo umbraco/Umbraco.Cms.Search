d0d8d81823871d2c7fe0b772fab53cbc2ba35fe8
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2025-04-27 14:33:37 +0200

    Create inital tests that just proves we can index some data

--- src/Umbraco.Test.Search.Examine.Integration/Tests/IndexServiceTests.cs
+++ src/Umbraco.Test.Search.Examine.Integration/Tests/IndexServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Examine;
+using NUnit.Framework;
 using Umbraco.Cms.Tests.Common.Testing;
 using Umbraco.Cms.Tests.Integration.Testing;
 
@@ -8,9 +9,66 @@ namespace Umbraco.Test.Search.Examine.Integration.Tests;
 [UmbracoTest(Database = UmbracoTestOptions.Database.NewSchemaPerFixture)]
 public class IndexServiceTests : UmbracoIntegrationTest
 {
-    [Test]
-    public async Task CanIndexCustomData()
+    
+    public IExamineManager ExamineManager => GetRequiredService<IExamineManager>();
+    protected override void CustomTestSetup(IUmbracoBuilder builder)
     {
-        Assert.Pass();
+        base.CustomTestSetup(builder);
+        builder.Services.AddExamine();
+        builder.Services.AddExamineLuceneIndex("TestIndex");
+    }
+    
+    [Test]
+    public async Task CanIndexAnyData()
+    {
+        var index = GetIndex();
+        index.IndexItem(new ValueSet(
+            "test",
+            "Person",
+            new Dictionary<string, object>()
+            {
+                {"FirstName", "Nikolaj" },
+                {"LastName", "Geisle" },
+                {"Email", "nge@umbraco.dk" },
+                {"Age", 30}
+            }));
+
+        var results = index.Searcher.CreateQuery().All().Execute();
+        Assert.That(results.TotalItemCount, Is.EqualTo(1));
+        Assert.That(results.First().Id, Is.EqualTo("test"));
+        
+    }
+    
+    [Test]
+    public async Task CanIndexData()
+    {
+        IndexData();
+        var index = GetIndex();
+        var results = index.Searcher.CreateQuery().All().Execute();
+        Assert.That(results.TotalItemCount, Is.EqualTo(3));
+        
+    }
+
+    public void IndexData(int count = 3)
+    {
+        var index = GetIndex();
+        for (int i = 0; i < count; i++)
+        {
+            index.IndexItem(new ValueSet(
+                $"TestId{i}",
+                "Person",
+                new Dictionary<string, object>()
+                {
+                    {"FirstName", $"FirstName{i}" },
+                    {"LastName",  $"LastName{i}" },
+                    {"Age", i },
+                }));
+        }
+    }
+
+    private IIndex GetIndex()
+    {
+        ExamineManager.TryGetIndex("TestIndex", out IIndex index);
+        return index;
     }
 }
