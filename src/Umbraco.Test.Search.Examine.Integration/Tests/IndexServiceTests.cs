3d0d0515f25dc140f01f6692505a46307fa2d1b1
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2025-04-30 19:28:57 +0200

    Add tests for datetimes

--- src/Umbraco.Test.Search.Examine.Integration/Tests/IndexServiceTests.cs
+++ src/Umbraco.Test.Search.Examine.Integration/Tests/IndexServiceTests.cs
@@ -1,74 +1,22 @@
 ﻿using Examine;
-using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
-using Umbraco.Cms.Core;
-using Umbraco.Cms.Core.Cache;
-using Umbraco.Cms.Core.Models;
-using Umbraco.Cms.Core.Notifications;
-using Umbraco.Cms.Core.Services;
-using Umbraco.Cms.Core.Sync;
-using Umbraco.Cms.Infrastructure.HostedServices;
-using Umbraco.Cms.Search.Core.DependencyInjection;
-using Umbraco.Cms.Search.Core.Services;
-using Umbraco.Cms.Search.Core.Services.ContentIndexing;
-using Umbraco.Cms.Search.Provider.Examine.Services;
-using Umbraco.Cms.Tests.Common.Builders;
-using Umbraco.Cms.Tests.Common.Builders.Extensions;
-using Umbraco.Cms.Tests.Common.Testing;
-using Umbraco.Cms.Tests.Integration.Testing;
 
 namespace Umbraco.Test.Search.Examine.Integration.Tests;
 
-
-[TestFixture]
-[UmbracoTest(Database = UmbracoTestOptions.Database.NewSchemaPerTest)]
-public class IndexServiceTests : UmbracoIntegrationTest
+public class IndexServiceTests : IndexTestBase
 {
-    private Guid RootKey { get; } = Guid.NewGuid();
-    private IContentTypeService ContentTypeService => GetRequiredService<IContentTypeService>();
-
-    private IContentService ContentService => GetRequiredService<IContentService>();
-    private IExamineManager ExamineManager => GetRequiredService<IExamineManager>();
-    
-    protected override void CustomTestSetup(IUmbracoBuilder builder)
-    {
-        base.CustomTestSetup(builder);
-        
-        builder.Services.AddExamine();
-        builder.Services.AddSingleton<TestInMemoryDirectoryFactory>();
-        builder.Services.AddExamineLuceneIndex<TestIndex, TestInMemoryDirectoryFactory>(Cms.Search.Core.Constants.IndexAliases.DraftContent);
-        builder.Services.AddExamineLuceneIndex<TestIndex, TestInMemoryDirectoryFactory>(Cms.Search.Core.Constants.IndexAliases.PublishedContent);
-        builder.Services.AddTransient<IndexService>();
-        builder.Services.AddTransient<IIndexService, IndexService>();
-        builder.Services.AddTransient<ISearchService, SearchService>();
-        
-        builder.Services.Configure<Umbraco.Cms.Search.Core.Configuration.IndexOptions>(options =>
-        {
-            options.RegisterIndex<IndexService, IDraftContentChangeStrategy>(Cms.Search.Core.Constants.IndexAliases.DraftContent, UmbracoObjectTypes.Document);
-            options.RegisterIndex<IndexService, IPublishedContentChangeStrategy>(Cms.Search.Core.Constants.IndexAliases.PublishedContent, UmbracoObjectTypes.Document);
-        });
-
-        builder.AddSearchCore();
-        
-        builder.Services.AddUnique<IBackgroundTaskQueue, ImmediateBackgroundTaskQueue>();
-        builder.Services.AddUnique<IServerMessenger, LocalServerMessenger>();
-        
-        builder.AddNotificationHandler<ContentTreeChangeNotification, ContentTreeChangeDistributedCacheNotificationHandler>();
-        builder.AddNotificationHandler<MediaTreeChangeNotification, MediaTreeChangeDistributedCacheNotificationHandler>();
-        builder.AddNotificationHandler<MemberSavedNotification, MemberSavedDistributedCacheNotificationHandler>();
-        builder.AddNotificationHandler<MemberDeletedNotification, MemberDeletedDistributedCacheNotificationHandler>();
-    }
-
     [Test]
     [TestCase(true)]
     [TestCase(false)]
     public void CanIndexAnyDocument(bool publish)
     {
-        CreateRootDocument(publish);
+        CreateInvariantRootDocument(publish);
         var content = ContentService.GetById(RootKey);
         Assert.That(content, Is.Not.Null);
 
-        var index = ExamineManager.GetIndex(publish ? Cms.Search.Core.Constants.IndexAliases.PublishedContent : Cms.Search.Core.Constants.IndexAliases.DraftContent);
+        var index = ExamineManager.GetIndex(publish
+            ? Cms.Search.Core.Constants.IndexAliases.PublishedContent
+            : Cms.Search.Core.Constants.IndexAliases.DraftContent);
 
         var results = index.Searcher.CreateQuery().All().Execute();
         Assert.That(results, Is.Not.Empty);
@@ -79,11 +27,13 @@ public class IndexServiceTests : UmbracoIntegrationTest
     [TestCase(false)]
     public void CanIndexTextProperty(bool publish)
     {
-        CreateRootDocument(publish);
+        CreateInvariantRootDocument(publish);
         var content = ContentService.GetById(RootKey);
         Assert.That(content, Is.Not.Null);
 
-        var index = ExamineManager.GetIndex(publish ? Cms.Search.Core.Constants.IndexAliases.PublishedContent : Cms.Search.Core.Constants.IndexAliases.DraftContent);
+        var index = ExamineManager.GetIndex(publish
+            ? Cms.Search.Core.Constants.IndexAliases.PublishedContent
+            : Cms.Search.Core.Constants.IndexAliases.DraftContent);
 
         var queryBuilder = index.Searcher.CreateQuery().All();
         queryBuilder.SelectField("title");
@@ -91,61 +41,44 @@ public class IndexServiceTests : UmbracoIntegrationTest
         Assert.That(results, Is.Not.Empty);
         Assert.That(results.First().Values.First().Value, Is.EqualTo("The root title"));
     }
-    
+
     [Test]
     [TestCase(true)]
     [TestCase(false)]
     public void CanIndexIntegerValues(bool publish)
     {
-        CreateRootDocument(publish);
+        CreateInvariantRootDocument(publish);
         var content = ContentService.GetById(RootKey);
         Assert.That(content, Is.Not.Null);
 
-        var index = ExamineManager.GetIndex(publish ? Cms.Search.Core.Constants.IndexAliases.PublishedContent : Cms.Search.Core.Constants.IndexAliases.DraftContent);
+        var index = ExamineManager.GetIndex(publish
+            ? Cms.Search.Core.Constants.IndexAliases.PublishedContent
+            : Cms.Search.Core.Constants.IndexAliases.DraftContent);
 
         var queryBuilder = index.Searcher.CreateQuery().All();
         queryBuilder.SelectField("count");
         var results = queryBuilder.Execute();
         Assert.That(results, Is.Not.Empty);
         Assert.That(results.First().Values.First().Value, Is.EqualTo("12"));
-    }
-
-    private void CreateRootDocument(bool publish = false)
+    }    
+    
+    [Test]
+    [TestCase(true)]
+    [TestCase(false)]
+    public void CanIndexDateTimeValues(bool publish)
     {
-        var contentType = new ContentTypeBuilder()
-            .WithAlias("invariant")
-            .AddPropertyType()
-            .WithAlias("title")
-            .WithDataTypeId(Constants.DataTypes.Textbox)
-            .WithPropertyEditorAlias(Constants.PropertyEditors.Aliases.TextBox)
-            .Done()
-            .AddPropertyType()
-            .WithAlias("count")
-            .WithDataTypeId(-51)
-            .WithPropertyEditorAlias(Constants.PropertyEditors.Aliases.Integer)
-            .Done()
-            .Build();
-        ContentTypeService.Save(contentType);
+        CreateInvariantRootDocument(publish);
+        var content = ContentService.GetById(RootKey);
+        Assert.That(content, Is.Not.Null);
 
-        var root = new ContentBuilder()
-            .WithKey(RootKey)
-            .WithContentType(contentType)
-            .WithName("Root")
-            .WithPropertyValues(
-                new
-                {
-                    title = "The root title",
-                    count = 12
-                })
-            .Build();
+        var index = ExamineManager.GetIndex(publish
+            ? Cms.Search.Core.Constants.IndexAliases.PublishedContent
+            : Cms.Search.Core.Constants.IndexAliases.DraftContent);
 
-        if (publish)
-        {
-            ContentService.SaveAndPublish(root);
-        }
-        else
-        {
-            ContentService.Save(root);
-        }
+        var queryBuilder = index.Searcher.CreateQuery().All();
+        queryBuilder.SelectField("datetime");
+        var results = queryBuilder.Execute();
+        Assert.That(results, Is.Not.Empty);
+        Assert.That(results.First().Values.First().Value, Is.EqualTo(CurrentDateTimeOffset.ToString()));
     }
 }
---
+++ src/Umbraco.Test.Search.Examine.Integration/Tests/IndexTestBase.cs
@@ -0,0 +1,107 @@
+﻿using Examine;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Notifications;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Sync;
+using Umbraco.Cms.Infrastructure.HostedServices;
+using Umbraco.Cms.Search.Core.DependencyInjection;
+using Umbraco.Cms.Search.Core.Services;
+using Umbraco.Cms.Search.Core.Services.ContentIndexing;
+using Umbraco.Cms.Search.Provider.Examine.Services;
+using Umbraco.Cms.Tests.Common.Builders;
+using Umbraco.Cms.Tests.Common.Builders.Extensions;
+using Umbraco.Cms.Tests.Common.Testing;
+using Umbraco.Cms.Tests.Integration.Testing;
+
+namespace Umbraco.Test.Search.Examine.Integration.Tests;
+
+[TestFixture]
+[UmbracoTest(Database = UmbracoTestOptions.Database.NewSchemaPerTest)]
+public class IndexTestBase : UmbracoIntegrationTest
+{
+    protected DateTimeOffset CurrentDateTimeOffset = DateTimeOffset.UtcNow;
+    protected Guid RootKey { get; } = Guid.NewGuid();
+    protected IContentTypeService ContentTypeService => GetRequiredService<IContentTypeService>();
+
+    protected IContentService ContentService => GetRequiredService<IContentService>();
+    protected IExamineManager ExamineManager => GetRequiredService<IExamineManager>();
+    
+    protected override void CustomTestSetup(IUmbracoBuilder builder)
+    {
+        base.CustomTestSetup(builder);
+        
+        builder.Services.AddExamine();
+        builder.Services.AddSingleton<TestInMemoryDirectoryFactory>();
+        builder.Services.AddExamineLuceneIndex<TestIndex, TestInMemoryDirectoryFactory>(Cms.Search.Core.Constants.IndexAliases.DraftContent);
+        builder.Services.AddExamineLuceneIndex<TestIndex, TestInMemoryDirectoryFactory>(Cms.Search.Core.Constants.IndexAliases.PublishedContent);
+        builder.Services.AddTransient<IndexService>();
+        builder.Services.AddTransient<IIndexService, IndexService>();
+        builder.Services.AddTransient<ISearchService, SearchService>();
+        
+        builder.Services.Configure<Umbraco.Cms.Search.Core.Configuration.IndexOptions>(options =>
+        {
+            options.RegisterIndex<IndexService, IDraftContentChangeStrategy>(Cms.Search.Core.Constants.IndexAliases.DraftContent, UmbracoObjectTypes.Document);
+            options.RegisterIndex<IndexService, IPublishedContentChangeStrategy>(Cms.Search.Core.Constants.IndexAliases.PublishedContent, UmbracoObjectTypes.Document);
+        });
+
+        builder.AddSearchCore();
+        
+        builder.Services.AddUnique<IBackgroundTaskQueue, ImmediateBackgroundTaskQueue>();
+        builder.Services.AddUnique<IServerMessenger, LocalServerMessenger>();
+        
+        builder.AddNotificationHandler<ContentTreeChangeNotification, ContentTreeChangeDistributedCacheNotificationHandler>();
+        builder.AddNotificationHandler<MediaTreeChangeNotification, MediaTreeChangeDistributedCacheNotificationHandler>();
+        builder.AddNotificationHandler<MemberSavedNotification, MemberSavedDistributedCacheNotificationHandler>();
+        builder.AddNotificationHandler<MemberDeletedNotification, MemberDeletedDistributedCacheNotificationHandler>();
+    }
+    
+    protected void CreateInvariantRootDocument(bool publish = false)
+    {
+        var contentType = new ContentTypeBuilder()
+            .WithAlias("invariant")
+            .AddPropertyType()
+            .WithAlias("title")
+            .WithDataTypeId(Constants.DataTypes.Textbox)
+            .WithPropertyEditorAlias(Constants.PropertyEditors.Aliases.TextBox)
+            .Done()
+            .AddPropertyType()
+            .WithAlias("count")
+            .WithDataTypeId(-51)
+            .WithPropertyEditorAlias(Constants.PropertyEditors.Aliases.Integer)
+            .Done()
+            .AddPropertyType()
+            .WithAlias("datetime")
+            .WithDataTypeId(Constants.DataTypes.DateTime)
+            .WithPropertyEditorAlias(Constants.PropertyEditors.Aliases.DateTime)
+            .Done()
+            .Build();
+        ContentTypeService.Save(contentType);
+
+        var root = new ContentBuilder()
+            .WithKey(RootKey)
+            .WithContentType(contentType)
+            .WithName("Root")
+            .WithPropertyValues(
+                new
+                {
+                    title = "The root title",
+                    count = 12,
+                    datetime = CurrentDateTimeOffset.DateTime,
+                })
+            .Build();
+
+        if (publish)
+        {
+            ContentService.SaveAndPublish(root);
+        }
+        else
+        {
+            ContentService.Save(root);
+        }
+    }
+    
+}
