2f1d5135e4e708bf568a5989524d4401bb636822
kjac <kja@umbraco.dk>
2025-07-21 15:37:43 +0200

    Removed the Score constant from FieldNames as it's not a field

--- src/Umbraco.Cms.Search.Core/Models/Searching/Sorting/ScoreSorter.cs
+++ src/Umbraco.Cms.Search.Core/Models/Searching/Sorting/ScoreSorter.cs
@@ -2,6 +2,6 @@ using Umbraco.Cms.Core;
 
 namespace Umbraco.Cms.Search.Core.Models.Searching.Sorting;
 
-public record ScoreSorter(Direction Direction) : Sorter(Constants.FieldNames.Score, Direction)
+public record ScoreSorter(Direction Direction) : Sorter(string.Empty, Direction)
 {
 }
