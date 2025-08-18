4741e411fcc9f9b9fc7a4556c5ba592b64cd5756
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2025-08-18 19:57:52 +0200

    Fix properties being indexed more than once for variance

--- src/Umbraco.Cms.Search.Provider.Examine/Services/Indexer.cs
+++ src/Umbraco.Cms.Search.Provider.Examine/Services/Indexer.cs
@@ -1,25 +1,24 @@
 ﻿using Examine;
 using Microsoft.Extensions.Logging;
 using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.PublishedCache;
 using Umbraco.Cms.Search.Core.Models.Indexing;
 using Umbraco.Cms.Search.Provider.Examine.Extensions;
 
 namespace Umbraco.Cms.Search.Provider.Examine.Services;
 
-public class Indexer(IExamineManager examineManager, ILogger<Indexer> logger) : IExamineIndexer
+public class Indexer(IExamineManager examineManager, IDefaultCultureAccessor defaultCultureAccessor, ILogger<Indexer> logger) : IExamineIndexer
 {
     public Task AddOrUpdateAsync(string indexAlias, Guid key, UmbracoObjectTypes objectType, IEnumerable<Variation> variations,
         IEnumerable<IndexField> fields, ContentProtection? protection)
     {
         var index = GetIndex(indexAlias);
-        
-        index.IndexingError += IndexingError;
 
         DeleteSingleDoc(index, key);
 
         var valuesToIndex = new List<ValueSet>();
 
-        foreach (var variation in variations)
+        foreach (Variation variation in variations)
         {
             var indexKey = CalculateIndexKey(key, variation);
             valuesToIndex.Add(new ValueSet(
@@ -27,10 +26,9 @@ public class Indexer(IExamineManager examineManager, ILogger<Indexer> logger) : 
                 objectType.ToString(),
                 MapToDictionary(fields.Where(x => (x.Culture is null && x.Segment is null) || (x.Culture == variation.Culture && x.Segment == variation.Segment)), variation.Culture, variation.Segment, protection)));
         }
-        
+
         index.IndexItems(valuesToIndex);
-        
-        // index.IndexingError -= IndexingError;
+
         return Task.CompletedTask;
     }
 
@@ -43,7 +41,7 @@ public class Indexer(IExamineManager examineManager, ILogger<Indexer> logger) : 
         index?.CreateIndex();
         return Task.CompletedTask;
     }
-    
+
     private static string CalculateIndexKey(Guid key, Variation variation)
     {
         var result = key.ToString().ToLowerInvariant();
@@ -56,16 +54,16 @@ public class Indexer(IExamineManager examineManager, ILogger<Indexer> logger) : 
         {
             result += $"_{variation.Segment}";
         }
-        
+
         return result;
     }
 
     private void DeleteSingleDoc(IIndex index, Guid key)
     {
         var documents = index.Searcher.CreateQuery().Field("Umb_Id_keywords", key.ToString().TransformDashes()).Execute();
-        
+
         var idsToDelete = new HashSet<string>();
-        
+
         foreach (var document in documents)
         {
             idsToDelete.Add(document.Id);
@@ -76,18 +74,12 @@ public class Indexer(IExamineManager examineManager, ILogger<Indexer> logger) : 
             index.DeleteFromIndex(idsToDelete);
         }
     }
-    
-    private void IndexingError(object? sender, IndexingErrorEventArgs e)
-    {
-        logger.LogCritical(e.Message, e);
-    }
-    
-    
+
     public async Task DeleteAsync(string indexAlias, IEnumerable<Guid> keys)
     {
         var index = GetIndex(indexAlias);
         var idsToDelete = new HashSet<string>();
-        
+
         foreach (var key in keys)
         {
             var documents = index.Searcher.Search(key.ToString());
@@ -95,14 +87,14 @@ public class Indexer(IExamineManager examineManager, ILogger<Indexer> logger) : 
             {
                 idsToDelete.Add(document.Id);
             }
-            
+
             var descendants = index.Searcher.CreateQuery().Field("Umb_PathIds_keywords", key.ToString()).Execute();
-            
+
             foreach (var descendant in descendants)
             {
                 idsToDelete.Add(descendant.Id);
             }
-            
+
             index.DeleteFromIndex(idsToDelete);
         }
     }
@@ -117,24 +109,31 @@ public class Indexer(IExamineManager examineManager, ILogger<Indexer> logger) : 
         List<string> aggregatedR3Texts = [];
         foreach (var field in fields)
         {
+            if (field.FieldName.StartsWith(Constants.Fields.FieldPrefix) is false && (culture is not null || segment is not null))
+            {
+                if ((field.Culture != culture && culture != defaultCultureAccessor.DefaultCulture)  || field.Segment != segment)
+                {
+                    continue;
+                }
+            }
             if (field.Value.Integers?.Any() ?? false)
             {
                 List<object> integers = field.Value.Integers.Cast<object>().ToList();
                 result.Add(CalculateFieldName(field, Constants.Fields.Integers),  integers);
             }
-            
+
             if (field.Value.Keywords?.Any() ?? false)
             {
                 result.Add(CalculateFieldName(field, Constants.Fields.Keywords), field.Value.Keywords.Select(x => x.TransformDashes()));
                 aggregatedTexts.AddRange(field.Value.Keywords);
             }
-            
+
             if (field.Value.Decimals?.Any() ?? false)
             {
                 List<object> decimals = field.Value.Decimals.Cast<object>().ToList();
                 result.Add(CalculateFieldName(field, Constants.Fields.Decimals), decimals);
             }
-            
+
             if (field.Value.DateTimeOffsets?.Any() ?? false)
             {
                 // We have to use DateTime here, as examine facets does not play nice with DatetimeOffsets for now.
@@ -145,22 +144,22 @@ public class Indexer(IExamineManager examineManager, ILogger<Indexer> logger) : 
             {
                 result.Add(CalculateFieldName(field, Constants.Fields.Texts), field.Value.Texts.Select(x => x.TransformDashes()));
                 aggregatedTexts.AddRange(field.Value.Texts);
-            }            
-            
+            }
+
             if (field.Value.TextsR1?.Any() ?? false)
             {
                 aggregatedR1Texts.AddRange(field.Value.TextsR1);
                 result.Add(CalculateFieldName(field, Constants.Fields.TextsR1), field.Value.TextsR1.Select(x => x.TransformDashes()));
                 aggregatedTexts.AddRange(field.Value.TextsR1);
             }
-            
+
             if (field.Value.TextsR2?.Any() ?? false)
             {
                 aggregatedR2Texts.AddRange(field.Value.TextsR2);
                 result.Add(CalculateFieldName(field, Constants.Fields.TextsR2), field.Value.TextsR2.Select(x => x.TransformDashes()));
                 aggregatedTexts.AddRange(field.Value.TextsR2);
             }
-            
+
             if (field.Value.TextsR3?.Any() ?? false)
             {
                 aggregatedR3Texts.AddRange(field.Value.TextsR3);
@@ -168,20 +167,20 @@ public class Indexer(IExamineManager examineManager, ILogger<Indexer> logger) : 
                 aggregatedTexts.AddRange(field.Value.TextsR3);
             }
         }
-        
+
         if (aggregatedTexts.Any())
         {
             result.Add($"{Constants.Fields.FieldPrefix}aggregated_texts", aggregatedTexts.ToArray());
         }
-        
+
         if (aggregatedR1Texts.Any())
         {
             result.Add($"{Constants.Fields.FieldPrefix}_{Constants.Fields.TextsR1}", aggregatedR1Texts.ToArray());
-        }     
+        }
         if (aggregatedR2Texts.Any())
         {
             result.Add($"{Constants.Fields.FieldPrefix}_{Constants.Fields.TextsR2}", aggregatedR2Texts.ToArray());
-        }       
+        }
         if (aggregatedR3Texts.Any())
         {
             result.Add($"{Constants.Fields.FieldPrefix}_{Constants.Fields.TextsR3}", aggregatedR3Texts.ToArray());
