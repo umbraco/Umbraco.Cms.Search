ea1bfc4b532154177231ee7476e8b8f2985ea3ea
kjac <kja@umbraco.dk>
2025-09-05 09:50:36 +0200

    Re-add required services for Examine after removing Examine

--- src/Umbraco.Cms.Search.BackOffice/DependencyInjection/RemoveExamineComposer.cs
+++ src/Umbraco.Cms.Search.BackOffice/DependencyInjection/RemoveExamineComposer.cs
@@ -1,6 +1,10 @@
+using Examine;
+using Examine.Lucene.Directories;
+using Microsoft.Extensions.DependencyInjection;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Cms.Infrastructure.Examine;
+using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Search.BackOffice.DependencyInjection;
 
@@ -9,5 +13,15 @@ public sealed class RemoveExamineComposer : IComposer
 {
     public void Compose(IUmbracoBuilder builder)
     {
+        IServiceCollection services = builder.Services;
+
+        // must re-add essential services and configuration for Examine to behave like it normally does
+
+        // - the actual ExamineManager (the default is a no-op)
+        services.AddUnique<IExamineManager, ExamineManager>();
+
+        // - the Umbraco implementations for app root and lock factory
+        services.AddSingleton<IApplicationRoot, UmbracoApplicationRoot>();
+        services.AddSingleton<ILockFactory, UmbracoLockFactory>();
     }
 }
