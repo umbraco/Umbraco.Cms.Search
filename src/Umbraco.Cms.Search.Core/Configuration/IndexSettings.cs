c575205cbd9ece41a92355746424fafe6a292d0e
Zeegaan <skrivdetud@gmail.com>
2025-11-11 19:45:40 +0900

    Add setting to only rebuild content indexes

---
+++ src/Umbraco.Cms.Search.Core/Configuration/IndexSettings.cs
@@ -0,0 +1,6 @@
+﻿namespace Umbraco.Cms.Search.Core.Configuration;
+
+public class IndexSettings
+{
+    public required string[] IncludeRebuildWhenLanguageDeleted { get; set; } = [Constants.IndexAliases.DraftContent, Constants.IndexAliases.PublishedContent];
+}
--- src/Umbraco.Cms.Search.Core/NotificationHandlers/RebuildIndexesNotificationHandler.cs
+++ src/Umbraco.Cms.Search.Core/NotificationHandlers/RebuildIndexesNotificationHandler.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Umbraco.Cms.Core.Events;
 using Umbraco.Cms.Core.Notifications;
+using Umbraco.Cms.Search.Core.Configuration;
 using Umbraco.Cms.Search.Core.Services.ContentIndexing;
 
 namespace Umbraco.Cms.Search.Core.NotificationHandlers;
@@ -9,13 +11,16 @@ internal sealed class RebuildIndexesNotificationHandler : INotificationHandler<U
 {
     private readonly IContentIndexingService _contentIndexingService;
     private readonly ILogger<RebuildIndexesNotificationHandler> _logger;
+    private readonly IndexSettings _options;
 
     public RebuildIndexesNotificationHandler(
         IContentIndexingService contentIndexingService,
-        ILogger<RebuildIndexesNotificationHandler> logger)
+        ILogger<RebuildIndexesNotificationHandler> logger,
+        IOptions<IndexSettings> options)
     {
         _contentIndexingService = contentIndexingService;
         _logger = logger;
+        _options = options.Value;
     }
 
     public void Handle(UmbracoApplicationStartedNotification notification)
@@ -29,10 +34,11 @@ internal sealed class RebuildIndexesNotificationHandler : INotificationHandler<U
 
     public void Handle(LanguageDeletedNotification notification)
     {
-        _logger.LogInformation("Rebuilding core search indexes...");
-        _contentIndexingService.Rebuild(Constants.IndexAliases.PublishedContent);
-        _contentIndexingService.Rebuild(Constants.IndexAliases.DraftContent);
-        _contentIndexingService.Rebuild(Constants.IndexAliases.DraftMedia);
-        _contentIndexingService.Rebuild(Constants.IndexAliases.DraftMembers);
+        _logger.LogInformation("Rebuilding search indexes after language deletion...");
+
+        foreach (var indexAlias in _options.IncludeRebuildWhenLanguageDeleted)
+        {
+            _contentIndexingService.Rebuild(indexAlias);
+        }
     }
 }
