9f59f8a31711b8ed987a8dff4a8c4bd850969c24
Zeegaan <skrivdetud@gmail.com>
2026-01-08 13:46:09 +0900

    Remove startup notification from core notification handler

--- src/Umbraco.Cms.Search.Core/DependencyInjection/UmbracoBuilderExtensions.cs
+++ src/Umbraco.Cms.Search.Core/DependencyInjection/UmbracoBuilderExtensions.cs
@@ -58,10 +58,4 @@ public static class UmbracoBuilderExtensions
 
         return builder;
     }
-
-    public static IUmbracoBuilder RebuildIndexesAfterStartup(this IUmbracoBuilder builder)
-    {
-        builder.AddNotificationHandler<UmbracoApplicationStartedNotification, RebuildIndexesNotificationHandler>();
-        return builder;
-    }
 }
