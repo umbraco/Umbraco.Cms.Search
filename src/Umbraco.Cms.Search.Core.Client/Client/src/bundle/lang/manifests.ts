9b88756c3304197b7c1006be92bffa888d673723
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2026-01-27 14:17:32 +0100

    adds Danish

--- src/Umbraco.Cms.Search.Core.Client/Client/src/bundle/lang/manifests.ts
+++ src/Umbraco.Cms.Search.Core.Client/Client/src/bundle/lang/manifests.ts
@@ -5,5 +5,12 @@ export const manifests: Array<UmbExtensionManifest> = [
     alias: 'Umbraco.Search.Localization.En',
     meta: { culture: 'en' },
     js: () => import('./en.js'),
+  },
+  {
+    type: 'localization',
+    name: 'Umbraco Search Localization - Danish',
+    alias: 'Umbraco.Search.Localization.Da',
+    meta: { culture: 'da' },
+    js: () => import('./da.js'),
   }
 ]
