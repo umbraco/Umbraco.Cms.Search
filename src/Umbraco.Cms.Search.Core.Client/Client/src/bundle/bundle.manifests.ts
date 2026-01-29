d1a9e6936cf2bedffd6ca5a26c1522d70aa993b4
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2026-01-29 15:49:37 +0100

    adds boxes to details view

--- src/Umbraco.Cms.Search.Core.Client/Client/src/bundle/bundle.manifests.ts
+++ src/Umbraco.Cms.Search.Core.Client/Client/src/bundle/bundle.manifests.ts
@@ -4,9 +4,12 @@ import { manifests as collectionViewManifests } from './collectionViews.manifest
 import { manifests as entityActionManifests } from './entityActions.manifests.js';
 import { manifests as globalContextManifests } from './globalContexts.manifests.js';
 import { manifests as repositoryManifests } from './repositories.manifests.js';
-import { manifests as workspaceViewManifests } from './workspaceViews.manifests.js';
+import { manifests as searchRootWorkspaceManifests } from './search-root-workspace.manifests.ts';
+import { manifests as searchWorkspaceManifests } from './search-workspace.manifests.ts';
+import { manifests as indexDetailBoxManifests } from './indexDetailBoxes.manifests.js';
+import { manifest as indexDetailBoxKindManifest } from './indexDetailBoxKind.manifests.js';
 
-import { UMB_SEARCH_ENTITY_TYPE } from '@umbraco-cms/search/global';
+import { UMB_SEARCH_ROOT_ENTITY_TYPE } from '@umbraco-cms/search/global';
 import { UMB_ADVANCED_SETTINGS_MENU_ALIAS } from '@umbraco-cms/backoffice/settings'
 
 // Job of the bundle is to collate all the manifests from different parts of the extension and load other manifests
@@ -18,14 +21,17 @@ export const manifests: Array<UmbExtensionManifest> = [
   ...collectionActionManifests,
   ...collectionViewManifests,
   ...entityActionManifests,
-  ...workspaceViewManifests,
+  ...searchRootWorkspaceManifests,
+  ...searchWorkspaceManifests,
+  ...indexDetailBoxManifests,
+  indexDetailBoxKindManifest as any,
   {
     type: 'menuItem',
     name: 'Umbraco Search Root Menu Item',
     alias: 'Umbraco.Search.Root.MenuItem',
     meta: {
       label: '#search_treeHeader',
-      entityType: UMB_SEARCH_ENTITY_TYPE,
+      entityType: UMB_SEARCH_ROOT_ENTITY_TYPE,
       icon: 'icon-search',
       menus: [UMB_ADVANCED_SETTINGS_MENU_ALIAS],
     },
