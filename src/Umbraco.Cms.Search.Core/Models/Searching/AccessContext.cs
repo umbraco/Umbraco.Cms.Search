f48a906531711147dba295f20a432a47a74addfb
kjac <kja@umbraco.dk>
2026-04-24 10:43:13 +0200

    Make it possible to bypass the access context when querying

--- src/Umbraco.Cms.Search.Core/Models/Searching/AccessContext.cs
+++ src/Umbraco.Cms.Search.Core/Models/Searching/AccessContext.cs
@@ -1,5 +1,6 @@
 ﻿namespace Umbraco.Cms.Search.Core.Models.Searching;
 
-public record AccessContext(Guid PrincipalId, Guid[]? GroupIds)
+public record AccessContext(Guid PrincipalId, Guid[]? GroupIds, bool Ignore = false)
 {
+    public static AccessContext Ignored() => new(Guid.Empty, null, true);
 }
