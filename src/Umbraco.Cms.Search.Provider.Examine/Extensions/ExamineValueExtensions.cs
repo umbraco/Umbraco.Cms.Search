60abc4820836ee91dd248709f1389bad6b6f8d33
kjac <kja@umbraco.dk>
2026-05-27 10:11:51 +0200

    Update Examine to beta.4

--- src/Umbraco.Cms.Search.Provider.Examine/Extensions/ExamineValueExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using Examine.Search;
-
-namespace Umbraco.Cms.Search.Provider.Examine.Extensions;
-
-// TODO: remove this with the next version of Examine
-internal static class ExamineValueExtensions
-{
-    public static IExamineValue WithBoost(this IExamineValue value, float _) => value;
-}
